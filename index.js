283deba6f416c3713cdcf726d201c308b66bd55b
Your Name <kkamadi567@gmail.com>
2023-10-01 20:57:51 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){    
+let p = document.querySelector("p"); 
+p.textContent = ("This is really cool!");
+// console.log("The DOM has loaded");
+});
+// text.style.textContent = "This is really cool!"
+
